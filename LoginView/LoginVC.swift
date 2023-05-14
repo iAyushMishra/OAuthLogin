aaa3e21033a003e58b2070a55864cd9fbe9fa173
MishraAyush <amishra.162@gmail.com>
2023-05-14 18:36:27 +0530

    Login View setup
    1. Setup LoginVC and connect IBAction button to view.
    2. Setup initial VC

---
+++ LoginView/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  OAuthLogin
+//
+//  Created by Ayush Mishra on 14/05/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @IBAction func btnLoginClicked(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
